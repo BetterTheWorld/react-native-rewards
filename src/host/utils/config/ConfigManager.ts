a862c8e5904e624fe9bb5f670c5e89635038f924
Noroboto <danieldiazg01@gmail.com>
2024-07-09 15:50:38 -0400

    feat: prevent store key collisions

--- src/host/utils/config/ConfigManager.ts
+++ src/host/utils/config/ConfigManager.ts
@@ -1,24 +1,43 @@
 import { hostColors } from '../../styles/colors';
-
-const defaultHostColors = hostColors;
+import type { RewardsTypes } from '../../types/modules';
 
 class ConfigManager {
-  private colors: typeof defaultHostColors;
+  private static instance: ConfigManager | null = null;
+  private colors: typeof hostColors;
+  private keys: RewardsTypes['keys'];
+  private partnerId: string;
 
-  constructor() {
-    this.colors = defaultHostColors;
+  private constructor(config: RewardsTypes['keys']) {
+    this.colors = hostColors;
+    this.keys = config;
+    this.partnerId = config.REWARDS_PROPS_X_REWARDS_PARTNER_ID;
+  }
+
+  public static getInstance(config?: RewardsTypes['keys']): ConfigManager {
+    if (!ConfigManager.instance && config) {
+      ConfigManager.instance = new ConfigManager(config);
+    }
+    return ConfigManager.instance!;
   }
 
   public getColors() {
     return this.colors;
   }
 
-  public updateColors(newColors?: Partial<typeof defaultHostColors>) {
+  public updateColors(newColors?: Partial<typeof hostColors>) {
     if (newColors) {
       this.colors = { ...this.colors, ...newColors };
     } else {
-      this.colors = defaultHostColors;
+      this.colors = hostColors;
     }
+  }
+
+  public getKeys() {
+    return this.keys;
+  }
+
+  public getPartnerId() {
+    return this.partnerId;
   }
 }
 
