8f5ca2cc40fcdd9a1e6477900156556c113d8eff
Noroboto <danieldiazg01@gmail.com>
2024-07-16 11:59:26 -0400

    chore: update exported types

--- src/index/types/index.ts
+++ src/index/types/index.ts
@@ -1,3 +1,4 @@
 export type * from '../../host/types/forms';
 export type * from '../../host/types/fields';
 export type { HostContextType } from '../../host/context/HostContext';
+export type * from '../../host/types/context';
