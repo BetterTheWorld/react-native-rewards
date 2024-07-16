bfafdfe0d76bcbdfe4c005e5dea65824cbb54142
Noroboto <danieldiazg01@gmail.com>
2024-07-16 12:10:28 -0400

    chore: update xontext type export

--- src/index/types/index.ts
+++ src/index/types/index.ts
@@ -1,4 +1,5 @@
 export type * from '../../host/types/forms';
 export type * from '../../host/types/fields';
 export type { HostContextType } from '../../host/context/HostContext';
-export type * from '../../host/types/context';
+export { UIStateType, TokenStage } from '../../host/types/context';
+export type { TokenInput } from '../../host/types/context';
