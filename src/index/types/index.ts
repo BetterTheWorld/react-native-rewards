54a39fd5e1b7887145a27c5e5369cb2a913b2519
Noroboto <danieldiazg01@gmail.com>
2025-02-07 11:45:33 -0500

    feat: export event messaging interface

--- src/index/types/index.ts
+++ src/index/types/index.ts
@@ -4,3 +4,4 @@ export type { HostContextType } from '../../host/context/HostContext';
 export { UIStateType, TokenStage } from '../../host/types/context';
 export type { TokenInput } from '../../host/types/context';
 export type * from '../../host/types/misc';
+export type * from '../../host/types/messages';
